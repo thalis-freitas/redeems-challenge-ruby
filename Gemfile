5ce08e21149a167fb7030dc45f16934b3a2332a4
Thalis Freitas <ofthalis@gmail.com>
2024-11-05 16:32:47 -0300

    Configure postgres and add volumes in docker

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source "https://rubygems.org"
 ruby "3.2.3"
 
 gem "rails", "~> 7.1.3"
-gem "sqlite3", "~> 1.4"
+gem "pg", "~> 1.4"
 gem "puma", ">= 5.0"
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 gem "bootsnap", require: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_10_29_201542) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "redeem_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
