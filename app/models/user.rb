4d7b151086f16173726ffbdd0a3b18fe9b377e85
Thalis Freitas <ofthalis@gmail.com>
2024-11-09 11:32:23 -0300

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,44 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :registration_number, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP,
+                              message: I18n.t('errors.messages.invalid_email') }
+
+  validate :valid_cpf
+
+  before_save :sanitize_registration_number
+
+  private
+
+  def valid_cpf
+    return if registration_number.blank?
+
+    unless cpf_valid?(registration_number)
+      errors.add(:registration_number, I18n.t('errors.messages.invalid_cpf'))
+    end
+  end
+
+  def cpf_valid?(cpf)
+    cpf = cpf.gsub(/[^\d]/, '')
+    return false if cpf.length != 11 || cpf.chars.uniq.size == 1
+
+    digit1 = calculate_cpf_digit(cpf[0..8])
+    digit2 = calculate_cpf_digit(cpf[0..9])
+    cpf[-2..-1] == "#{digit1}#{digit2}"
+  end
+
+  def calculate_cpf_digit(digits)
+    factor = digits.length + 1
+    sum = digits.chars.map(&:to_i).each_with_index.sum do |digit, index|
+      digit * (factor - index)
+    end
+
+    result = 11 - (sum % 11)
+    result > 9 ? 0 : result
+  end
+
+  def sanitize_registration_number
+    self.registration_number = registration_number.gsub(/\D/, '')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_06_205421) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_09_134241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,12 +84,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_06_205421) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "cpf", null: false
+    t.string "registration_number", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cpf"], name: "index_users_on_cpf", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["registration_number"], name: "index_users_on_registration_number", unique: true
   end
 
   add_foreign_key "answers", "questions"
